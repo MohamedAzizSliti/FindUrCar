fadff7f29610a3c6a0b60de5fd0c9fd94ab991be
zzsliti1999 <zzsliti1999@gmail.com>
2022-01-01 13:29:21 +0100

    azaazazazazazazaz

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                    startActivity(new Intent(SplashActivity.this, login.class));
+                    finish();
+                }
+        }, 2000);
+
+    }
+}
